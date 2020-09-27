4c8f96a516766c678ab980278c8f8a54f830ac89
mohtoka <mohamedtokaa@gmail.com>
2020-09-27 03:31:42 -0400

    FeatureB completed

---
+++ src/com/stash/FeatureB.java
@@ -0,0 +1,13 @@
+package com.stash;
+
+public class FeatureB {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("FeatureB");
+		
+		System.out.println("FeatureB completed");
+		
+	}
+
+}
